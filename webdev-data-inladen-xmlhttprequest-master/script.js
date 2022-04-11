cd7eb5a54e0cebb4b756ccef3e7f1060e2941f89
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-04-11 15:57:00 +0200

    Add files via upload

--- webdev-data-inladen-xmlhttprequest-master/script.js
+++ webdev-data-inladen-xmlhttprequest-master/script.js
@@ -1,44 +1,95 @@
 var xmlhttp = new XMLHttpRequest();
-var url = './data/data.json';
+var url = "./data/data.json";
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
-        var myData = JSON.parse(this.responseText);
-        showData(myData)
-    }
-}
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    data = JSON.parse(this.responseText);
+    showData();
+  }
+};
 
-xmlhttp.open('GET', url, true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function showData(data){
-    var containerDiv = document.createElement('div');
-    containerDiv.className = 'flex-container';
+var current = 0;
+var data = null;
 
-    var ul = document.createElement('ul');
-    
-    for( var i =0; i<data.length; i++){
-        var li = document.createElement('li');
-        var itemDiv = document.createElement('div');
+function showData() {
+  var containerDiv = document.createElement("div");
+  containerDiv.className = "flex-container";
 
-        itemDiv.className = 'flex-items';
+  var previous = document.createElement("button");
+  previous.innerHTML = "previous";
+  previous.addEventListener("click", previousButton);
+  var next = document.createElement("button");
+  next.innerHTML = "next";
+  next.addEventListener("click", nextButton);
 
-        var pos = data[i].pos; 
-        var coureur = data[i].coureur;
-        var team  = data[i].team;
-        var tijd = data[i].tijd;
-        var punten  = data[i].punten;
-        
+  var ul = document.createElement("ul");
 
-        itemDiv.innerHTML = `<div> Position: ${pos}:${coureur} </div>
+  var li = document.createElement("li");
+  var itemDiv = document.createElement("div");
+
+
+  itemDiv.className = "flex-items";
+  itemDiv.id = 'currentItem';
+
+  var pos = data[0].pos;
+  var coureur = data[0].coureur;
+  var team = data[0].team;
+  var tijd = data[0].tijd;
+  var punten = data[0].punten;
+    var img = document.createElement("img");
+  img.className = "image_container";
+  img.src = data[0].afbeelding;
+
+  itemDiv.innerHTML = `<div> Position: ${pos}:${coureur} </div>
         <div> team: ${team} </div>
         <div> tijd: ${tijd} </div>
         <div> Punten: ${punten} </div>`;
-        li.appendChild(itemDiv);
-        ul.appendChild(li);
+  li.appendChild(itemDiv);
+  li.appendChild(img);
+  ul.appendChild(previous);
+  ul.appendChild(next);
+  ul.appendChild(li);
 
-    }
+  containerDiv.appendChild(ul);
+  document.body.appendChild(containerDiv);
+}
 
-    containerDiv.appendChild(ul);
-    document.body.appendChild(containerDiv);
+function nextButton() {
+  current++;
+  if(current > data.length-1){
+      current = 0;
+  }
+
+  
+  update(current);
+  console.log(current);
+}
+
+function previousButton() {
+  current--;
+  if( current <0){
+      current = data.length-1;
+  }
+  
+  update(current);
+  console.log(current);
+}
+
+function update(currentId) {
+  var pos = data[currentId].pos;
+  var coureur = data[currentId].coureur;
+  var team = data[currentId].team;
+  var tijd = data[currentId].tijd;
+  var punten = data[currentId].punten;
+
+  var div = document.getElementById("currentItem");
+
+  
+  div.innerHTML = `<div> Position: ${pos}:${coureur} </div>
+        <div> team: ${team} </div>
+        <div> tijd: ${tijd} </div>
+        <div> Punten: ${punten} </div>`;
 }
